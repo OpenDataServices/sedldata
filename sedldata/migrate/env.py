30a7cdf997e637a21789c61e187d5b11cd266736
Bibiana Cristofol Amat <bcristofol@gmail.com>
2018-10-08 12:45:54 +0100

    Try to get engine url

--- sedldata/migrate/env.py
+++ sedldata/migrate/env.py
@@ -1,14 +1,16 @@
 from __future__ import with_statement
+
+from logging.config import fileConfig
+
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-from logging.config import fileConfig
 
 import sedldata.database
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", str(sedldata.database.engine.url))
+config.set_main_option("sqlalchemy.url", str(sedldata.database.Database().engine.url))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
